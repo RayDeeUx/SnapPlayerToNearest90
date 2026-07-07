011dd9d8726463df5c091ac2136622f0d0e3c35b
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-07-07 17:21:04 -0400

    FICK

--- src/main.cpp
+++ src/main.cpp
@@ -125,7 +125,7 @@ class $modify(MyPlayerObject, PlayerObject) {
 		// auto v10 = (v8 != static_cast<GameObjectType>(static_cast<int>(GameObjectType::GravityTogglePortal) | static_cast<int>(GameObjectType::NormalGravityPortal))) & (v7 ^ 1);
 		// if ((!this->m_stateRingJump2 || this->m_isDashing || !this->m_stateJumpBuffered || this->m_touchedRing >= v10 && this->m_touchedCustomRing >= v7 && this->m_touchedGravityPortal >= v9) && !skipCheck) return;
 
-		if (!this->m_holdingButtons.at(static_cast<int>(PlayerButton::Jump)))) return;
+		if (!this->m_holdingButtons.at(static_cast<int>(PlayerButton::Jump))) return;
 
 		if (object->m_objectType == GameObjectType::YellowJumpRing && ignoreYellowOrb) return;
 		if (object->m_objectType == GameObjectType::PinkJumpRing && ignorePinkOrb) return;
